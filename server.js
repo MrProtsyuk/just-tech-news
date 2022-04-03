ded743d82ff56db4f000666b277a1ff5209e2082
Your Name <markprotsyuk@Marks-MacBook-Pro-2.local>
2022-04-03 16:27:46 -0700

    organized everything and made tests

--- server.js
+++ server.js
@@ -1,7 +1,8 @@
 const path = require('path');
 const express = require('express');
 const exphbs = require('express-handlebars');
-const hbs = exphbs.create({});
+const hbs = exphbs.create({ helpers });
+const helpers = require('./utils/helpers');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
